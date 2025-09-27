88152f68b67e0406fdfbc7b9fa39774174fc0584
becar-maker <aleksander.klancisar@gmail.com>
2025-09-27 19:22:22 +0200

    Update backend_interface.py

--- app/backend_interface.py
+++ app/backend_interface.py
@@ -1,1 +1,12 @@
+from abc import ABC, abstractmethod
+
+class MagnifierBackend(ABC):
+    @abstractmethod
+    def reset(self, levels, low, high, fps, shape_hw):
+        ...
+
+    @abstractmethod
+    def magnify(self, gray01, alpha):
+        """gray01: float32 [0..1], returns float32 [0..1]"""
+        ...
 
